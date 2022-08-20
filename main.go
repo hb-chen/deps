2cc5ba8a2fffde6d0914a93d06f7d4e63e47641e
hbchen <hobo.chb@gmail.com>
2022-08-20 17:08:35 +0800

    add go generate for go-bindata

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import "github.com/hb-chen/deps/cmd"
 
+//go:generate go-bindata -o ./pkg/output/template/bindata.go -pkg template -prefix template template
+
 func main() {
 	cmd.Execute()
 }
